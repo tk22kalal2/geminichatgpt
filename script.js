e25e48390e2e652b58b2e31c8030e779ecd70fa5
hello world <159936375+tk22kalal2@users.noreply.github.com>
2024-12-15 00:32:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ let isResponseGenerating = false;
 
 // API configuration
 const API_KEY = "AIzaSyDtN9czLTfmVm3FvC-MzL3fdbqIiHMGRxo"; // Your API key here
-const API_URL = `https://generativelanguage.googleapis.com/v1/models/gemini-1.5-pro:generateContent?key=${API_KEY}`;
+const API_URL = `https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent?key=${API_KEY}`;
 
 // Load theme and chat data from local storage on page load
 const loadDataFromLocalstorage = () => {
